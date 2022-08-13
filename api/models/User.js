3b2c7a754c5e5171c437730942bb1872a0c031c5
Thanh Le <thanhlevan0203@gmail.com>
2022-08-13 12:28:43 -0700

    Created Post models

--- api/models/User.js
+++ api/models/User.js
@@ -2,24 +2,24 @@ const mongoose = require("mongoose")
 
 const UserSchema = new mongoose.Schema(
     {
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    profilePic: {
-        type: String,
-        default: ""
-    },
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        profilePic: {
+            type: String,
+            default: ""
+        },
 
    },
    { timestamps: true }
