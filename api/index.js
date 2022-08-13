b539556e7ef1685a966a3a94db93c082524725b3
Thanh Le <thanhlevan0203@gmail.com>
2022-08-13 11:35:57 -0700

    Installed Mongoose DB

--- api/index.js
+++ api/index.js
@@ -3,22 +3,18 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
-const port = 8081;
-
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
 })
-.then(console.log("Connected to MongoDB"))
-.catch((err) => console.log(err));
+.then((res) => { console.log('Connected to MongoDB'); })
+.catch((err) => { console.log(err); });
 
-app.get("/", (req, res) => {
-   res.send("Hey this is main url");
-})
+const port = process.env.PORT || 8080
 
 app.listen(port, () => {
-    console.log("Backend is running on port ${port}");
+    console.log(`Backend is running on port ${port}`);
 })
