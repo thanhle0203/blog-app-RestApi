3c3704551d810790dc2877615ce8cdd24dd53ba7
Thanh Le <thanhlevan0203@gmail.com>
2022-08-13 14:53:18 -0700

    Added await

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -24,14 +24,14 @@ router.post("/register", async (req, res) => {
 // LOGIN 
 router.post("/login", async (req, res) => {
     try {
-        const user = User.findOne({
-            username: req.body.username})
-            !user && res.status(400).json("Wrong credentials!")
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(400).json("Wrong credentials!");
 
-            const validated = await bcrypt.compare(req.body.password, user.password)
-            !validated && res.status(400).json("Wrong credentials!");
+        const validated = await bcrypt.compare(req.body.password, user.password)
+        !validated && res.status(400).json("Wrong credentials!");
 
-            res.status(200).json(user);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
     } catch (err) {
         res.status(500).json(err);
     }
