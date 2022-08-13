dcf5f33612c1d3231d7555bf22df0406eb1d4cba
Thanh Le <thanhlevan0203@gmail.com>
2022-08-13 11:19:02 -0700

    Created env

--- api/index.js
+++ api/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-app.listen("6000", () => {
-    console.log("Backend is running");
+const port = 8081;
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
+.then(console.log("Connected to MongoDB"))
+.catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+   res.send("Hey this is main url");
+})
+
+app.listen(port, () => {
+    console.log("Backend is running on port ${port}");
 })
