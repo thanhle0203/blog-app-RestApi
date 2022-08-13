2a0c9b4192dfbaee4157b8372daaf392a4d26afc
Thanh Le <thanhlevan0203@gmail.com>
2022-08-13 15:09:46 -0700

    Created Update User

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
 
 dotenv.config();
 app.use(express.json());
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGO_URL,{
 .catch((err) => { console.log(err); });
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
 
 const port = process.env.PORT || 8080
 
---
+++ api/routes/users.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+// UPDATE USER
+router.put("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id) {
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10);
+            req.body
+        } 
+
+        try {
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json(updatedUser);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(401).json("You can update only your account!");
+    }
+    
+});
+
+// DELETE USER
+
+module.exports = router
